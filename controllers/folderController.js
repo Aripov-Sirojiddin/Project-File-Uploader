1000239c8a4b188ecf617a6e68fad3b9f54d340a
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-05 11:03:56 -0400

    Setting ground work for opening folders.

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -2,12 +2,16 @@ const folderModel = require("../models/folders");
 
 async function getCreateView(req, res) {
   const folders = await folderModel.getAllByParentId(global.parentId);
-
   res.render("pages/logedin", {
     name: req.user.name,
     createFolder: true,
     folders: folders,
   });
+}
+
+async function openFolder(req, res) {
+  const folderId = req.param.folderId;
+  console.log(folderId);
 }
 
 async function createFolder(req, res) {
@@ -17,5 +21,6 @@ async function createFolder(req, res) {
 
 module.exports = {
   getCreateView,
+  openFolder,
   createFolder,
 };
--- routers/folderRouter.js
+++ routers/folderRouter.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const folderRouter = Router();
 const {
   getCreateView,
+  openFolder,
   createFolder,
 } = require("../controllers/folderController");
 
 folderRouter.get("/create", getCreateView);
+folderRouter.get("/:folderId", openFolder);
 folderRouter.post("/create", createFolder);
 
 module.exports = folderRouter;
