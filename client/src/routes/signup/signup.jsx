4ca4b0ec938842646d9f734521fed0a2f7e32c50
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-07 16:52:04 -0400

    Storing the user's last inputs so they are not reset when submitting the form.

--- client/src/routes/signup/signup.jsx
+++ client/src/routes/signup/signup.jsx
@@ -6,9 +6,15 @@ import { useState } from "react";
 export default function SignUp() {
   const navigate = useNavigate();
   const [errors, setErrors] = useState([]);
+  const [userInput, setUserInput] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
 
   async function createAccount(event) {
     const data = Object.fromEntries(event);
+
     await axios
       .post(`${import.meta.env.VITE_URL}/signup`, data)
       .then((response) => {
@@ -21,6 +27,16 @@ export default function SignUp() {
       });
   }
 
+  function updateUserInputs(event) {
+    const { name, value } = event.target;
+    setUserInput((oldInputs) => {
+      return {
+        ...oldInputs,
+        [name]: value,
+      };
+    });
+  }
+
   const errorsView = errors.map((error) => (
     <a href={`#${error.path}`}>{error.msg}</a>
   ));
@@ -28,25 +44,33 @@ export default function SignUp() {
   return (
     <div className={styles.vertical_container}>
       <h1>Create Account</h1>
-      {errors.length > 0 && <div className={styles.error_box}>{errorsView}</div>}
+      {errors.length > 0 && (
+        <div className={styles.error_box}>{errorsView}</div>
+      )}
       <form action={createAccount}>
         <input
           id="firstname"
           name="firstname"
           placeholder="Firstname"
           type="text"
+          value={userInput.firstname}
+          onChange={updateUserInputs}
         />
         <input
           id="lastname"
           name="lastname"
           placeholder="Lastname"
           type="text"
+          value={userInput.lastname}
+          onChange={updateUserInputs}
         />
         <input
           id="email"
           name="email"
           placeholder="Email (example@example.com)"
           type="text"
+          value={userInput.email}
+          onChange={updateUserInputs}
         />
         <input
           id="password"
