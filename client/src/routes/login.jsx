b5bca3d08da3e94d097cf6de1afdf8ee754ac22d
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-07 15:51:36 -0400

    Displaying message to user when provided incorrect login information.

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [error, setError] = useState("");
+
   async function loginUser(form) {
     const result = await axios
       .post("http://localhost:3000/login", {
@@ -11,20 +14,24 @@ export default function LoginPage() {
         console.log(response);
       })
       .catch((error) => {
-        console.error(error);
+        if (error.response.status === 401) {
+          setError("Incorrect login information provided");
+        }
       });
-    console.log(result);
   }
 
   return (
-    <form action={loginUser}>
-      <input
-        type="text"
-        name="email"
-        placeholder="Email (example@example.com)"
-      />
-      <input type="password" name="password" placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <h1>{error}</h1>
+      <form action={loginUser}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email (example@example.com)"
+        />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </>
   );
 }
