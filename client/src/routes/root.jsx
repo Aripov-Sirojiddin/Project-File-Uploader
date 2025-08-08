10da6b2b4ff9a978d01b32605c8e8761fe1eaabe
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-08-08 16:42:02 -0400

    Authenication completed users can now login and logout.

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  function logout() {
+    localStorage.removeItem("token");
+    setToken(null);
+  }
   return (
     <>
-      <Link to="login">Login</Link>
+      {token ? (
+        <Link onClick={logout}>Logout</Link>
+      ) : (
+        <Link to="login">Login</Link>
+      )}
       <Outlet />
     </>
   );
